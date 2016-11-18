cf13a55bf93df7a81b5bee0ef098f2041deb7e70
jackson meyer <Guest@epicodus-32.local>
2016-11-18 11:37:43 -0800

    remove the switch and abort plan for now, working on list views and activities

--- app/src/main/java/com/example/guest/sidekick_wk_1/MainActivity.java
+++ app/src/main/java/com/example/guest/sidekick_wk_1/MainActivity.java
@@ -3,27 +3,40 @@ package com.example.guest.sidekick_wk_1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    @Bind(R.id.firstContinueButton) Button mfirstContinueButton;
+    private Button mfirstContinueButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+//        ButterKnife.bind(this);
 
+
+        mfirstContinueButton = (Button) findViewById(R.id.firstContinueButton);
         mfirstContinueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SurveyInfoPage.class);
                 startActivity(intent);
+                Toast.makeText(MainActivity.this, "Thanks for joining the team!", Toast.LENGTH_SHORT).show();
+
             }
         });
+
+
+
+
     }
 }
