a0f1e4737e976d99fca2792ebef3ac1d29cfa464
jackson meyer <Guest@epicodus-32.local>
2016-11-18 09:56:19 -0800

    added a second page, navigated there on click

--- app/src/main/java/com/example/guest/sidekick_wk_1/MainActivity.java
+++ app/src/main/java/com/example/guest/sidekick_wk_1/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.guest.sidekick_wk_1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.firstContinueButton) Button mfirstContinueButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mfirstContinueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SurveyInfoPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/guest/sidekick_wk_1/SurveyInfoPage.java
@@ -0,0 +1,13 @@
+package com.example.guest.sidekick_wk_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SurveyInfoPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survey_info_page);
+    }
+}
