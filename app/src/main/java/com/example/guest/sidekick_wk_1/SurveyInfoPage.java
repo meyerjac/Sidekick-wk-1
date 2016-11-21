3204d1d83b65354a542c142127dd7285cb7cc73f
jackson Meyer <jacksonmeyer@jacksons-MacBook-Pro.local>
2016-11-21 11:43:13 -0800

    add toasts and a bit of styling

--- app/src/main/java/com/example/guest/sidekick_wk_1/SurveyInfoPage.java
+++ app/src/main/java/com/example/guest/sidekick_wk_1/SurveyInfoPage.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -15,12 +16,16 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class SurveyInfoPage extends AppCompatActivity {
-    @Bind(R.id.secondPageButton) Button msecondPageButton;
-    @Bind(R.id.nameInput) EditText mnameInput;
-    @Bind(R.id.emailInput) EditText memailInput;
-    @Bind(R.id.passwordInput) EditText mpasswordInput;
-    @Bind(R.id.ageInput) EditText mageInput;
-
+    @Bind(R.id.secondPageButton)
+    Button msecondPageButton;
+    @Bind(R.id.nameInput)
+    EditText mnameInput;
+    @Bind(R.id.emailInput)
+    EditText memailInput;
+    @Bind(R.id.passwordInput)
+    EditText mpasswordInput;
+    @Bind(R.id.ageInput)
+    EditText mageInput;
 
 
     @Override
@@ -31,19 +36,28 @@ public class SurveyInfoPage extends AppCompatActivity {
 
 
         msecondPageButton.setOnClickListener(new View.OnClickListener() {
+
+
+
             @Override
             public void onClick(View v) {
-                String password = mpasswordInput.getText().toString();
-                String age = mageInput.getText().toString();
-                String name = mnameInput.getText().toString();
-                String email = memailInput.getText().toString();
-                Intent intent = new Intent(SurveyInfoPage.this, listOfPeopleActivity.class);
-                intent.putExtra("password", password);
-                intent.putExtra("age", age);
-                intent.putExtra("name", name);
-                intent.putExtra("email", email);
-                startActivity(intent);
-                }
+                if ((mpasswordInput.getText().toString() == "") || (mageInput.getText().toString()=="") ||
+                        (mnameInput.getText().toString()=="") || (memailInput.getText().toString()=="")) {
+                    Toast.makeText(SurveyInfoPage.this, "please fill out all the text fields!", Toast.LENGTH_SHORT).show();
+                } else {
+                    String password = mpasswordInput.getText().toString();
+                    String age = mageInput.getText().toString();
+                    String name = mnameInput.getText().toString();
+                    String email = memailInput.getText().toString();
+                    Intent intent = new Intent(SurveyInfoPage.this, listOfPeopleActivity.class);
+                    intent.putExtra("password", password);
+                    intent.putExtra("age", age);
+                    intent.putExtra("name", name);
+                    intent.putExtra("email", email);
+                    startActivity(intent);
+                    }
+                };
             });
-    }
+        };
 }
+
